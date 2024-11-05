60701cb6329b6fb676ae407760e58025409601f3
MRP-rakib <rakib260.hossain@gmail.com>
2024-11-05 15:24:09 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif',],
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'selector',
+}
+
